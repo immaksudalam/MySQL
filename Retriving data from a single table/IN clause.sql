61424b80a05dab80533b98d923d52852b814cf2f
Maksud Alam Rony <53002641+immaksudalam@users.noreply.github.com>
2022-04-07 05:51:31 -0700

    Create IN clause.sql

---
+++ Retriving data from a single table/IN clause.sql
@@ -0,0 +1,13 @@
+use student;
+
+select *
+from result
+where name in ("maksud alam", "sony", "jakariya")
+
+
+use student;
+
+select *
+from result
+where name NOT in ("maksud alam", "sony", "jakariya")
+
