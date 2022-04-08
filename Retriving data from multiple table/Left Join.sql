b73c17d7ef5a93963820179e04297f69beb05e72
Maksud Alam Rony <53002641+immaksudalam@users.noreply.github.com>
2022-04-08 01:49:44 -0700

    Create Left Join.sql

---
+++ Retriving data from multiple table/Left Join.sql
@@ -0,0 +1,7 @@
+use my_database;
+
+select customers.name as customer_name , orders.order_date as date
+from customers
+Left  JOIN  orders 
+ON customers.customer_id = orders.customer_id
+where customers.birth_date between "1997-01-01" and "1997-12-30";
